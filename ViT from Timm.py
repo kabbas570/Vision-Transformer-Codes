1890c92c5bd49fdad7765376ea57543246ec0c1d
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2022-10-27 16:55:36 +0100

    Update ViT from Timm.py

--- ViT from Timm.py
+++ ViT from Timm.py
@@ -2,24 +2,56 @@ import torch
 import torch.nn as nn
 import timm
 
-model_name = "vit_base_patch16_224"
-model = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=3)
 
-#  Freeze the base parameters
-for parameter in model.parameters():
-    parameter.requires_grad = False
+### Trainable params: 3,843  initialized from pretrained ###
+def ViT_1():
+    model = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=3)
+    
+    #  Freeze the base parameters
+    for parameter in model.parameters():
+        parameter.requires_grad = False
+    
+    #params = model.state_dict()
+    #params.keys()
+    
+    model.norm.weight.requires_grad = True
+    model.norm.bias.requires_grad = True
+    
+    model.head.weight.requires_grad = True
+    model.head.bias.requires_grad = True
+    
+    return model
 
-#params = model.state_dict()
-#params.keys()
-
-model.norm.weight.requires_grad = True
-model.norm.bias.requires_grad = True
-
-model.head.weight.requires_grad = True
-model.head.bias.requires_grad = True
-
+model=ViT_1()
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 from torchsummary import summary
 model.to(device=DEVICE,dtype=torch.float)
 summary(model, (3, 224, 224))
 
+### Trainable params: 85,648,899  initialized Randamly ###
+def ViT_2():
+
+    model = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=3)    
+    
+    return model
+
+model=ViT_2()
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+from torchsummary import summary
+model.to(device=DEVICE,dtype=torch.float)
+summary(model, (3, 224, 224))
+
+
+### Trainable params: 85,648,899  initialized from pretrained ###
+
+def ViT_3():
+
+    model = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=3)    
+    
+    return model
+
+model=ViT_3()
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+from torchsummary import summary
+model.to(device=DEVICE,dtype=torch.float)
+summary(model, (3, 224, 224))
